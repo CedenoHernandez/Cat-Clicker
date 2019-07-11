98ff1740cec746f93d726f4ce822a7ab84e48cfc
Cedeño Hernandez <cedeno.hernandez@gmail.com>
2019-07-11 15:33:48 -0700

    refactor: implement MOV
    Begin version 2 of app. Increased functionality with cat selection will be added but code needs to be refactored. JavaScript changed to separate concerns of view, model, and octopus.

--- Cat Clicker/app.js
+++ Cat Clicker/app.js
@@ -1,8 +1,66 @@
-const cat = document.querySelector('img');
-const counter = document.querySelector('.clicks');
-let clicks = 0;
 
-cat.addEventListener('click', function() {
-  clicks++;
-  counter.innerHTML = clicks;
-});
+let model = {
+  currentCat: null,
+  cats: [
+    {
+      name: "Theo",
+      clickCount: 0,
+      imgSrc: 'images/cute-cat1.png'
+    },
+    {
+      name: "Will",
+      clickCount: 0,
+      imgSrc: 'images/cute-cat2.png'
+    },
+    {
+      name: "Charles",
+      clickCount: 0,
+      imgSrc: 'images/cute-cat3.png'
+    },
+    {
+      name: "Edward",
+      clickCount: 0,
+      imgSrc: 'images/cute-cat4.png'
+    },
+    {
+      name: "Xavier",
+      clickCount: 0,
+      imgSrc: 'images/cute-cat5.png'
+    }
+  ]
+};
+
+
+
+let octopus = {
+  init: function() {
+    model.currentCat = model.cats[0];
+    catListView.init();
+    catView.init();
+  },
+  // WAY more code here
+};
+
+
+let catView = {
+  init: function() {
+    //massive code here
+  },
+  render: function() {
+    //massive code here
+  }
+};
+
+
+let catListView = {
+  init: function() {
+    // massive code here
+  },
+  render: function() {
+    // massive code here
+  }
+};
+
+
+//app starts!
+octopus.init();
